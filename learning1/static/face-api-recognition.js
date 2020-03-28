7353431c947a66a6a3f734b29932bf45a36f5f53
Nhan Phan <32973466+Enzophan@users.noreply.github.com>
2020-03-29 00:33:06 +0700

    Update

--- learning1/static/face-api-recognition.js
+++ learning1/static/face-api-recognition.js
@@ -41,13 +41,13 @@ async function start() {
 
 
 function loadLabeledImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark'];
+    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'Hulk'];
     return Promise.all(
         labels.map(async label => {
             const descriptions = [];
-            // https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/Black%20Widow/1.jpg
+            // https://raw.githubusercontent.com/Enzophan/tensorflowjs/master/learning1/static/labeled_images/Black%20Widow/1.jpg
             for (let i = 1; i <= 2; i++) {
-                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Enzophan/tensorflowjs/master/learning1/static/labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
 
                 descriptions.push(detections.descriptor)
